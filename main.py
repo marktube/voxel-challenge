44769434b3abd7c1eb86a080575072ce153b6f0f
marktube <liuyanchao99@gmail.com>
2022-05-02 19:05:28 +0800

    add heart

--- main.py
+++ main.py
@@ -11,10 +11,19 @@ scene.set_directional_light((1, 1, 0), 0.2, vec3(1.0, 1.0, 1.0) / exposure)
 scene.set_background_color(vec3(0.6, 0.8, 1.0) / exposure)
 
 @ti.func
-def create_moon(pos, radius, color):
-    for I in ti.grouped(ti.ndrange((-radius, radius), (-radius, radius), (-radius, radius))):
-        if I.norm() < radius:
-           scene.set_voxel(pos + I, 2, color)
+def create_heart(pos, sz, color):
+    for I in ti.grouped(ti.ndrange((-sz, sz), (-sz, sz))):
+        tmp = vec2([I[0]+0.5,I[1]+0.5])
+        tmp[0] = tmp[0] / sz * 4
+        tmp[1] = tmp[1] / sz * 4.5 - 1.5
+        pol_r = tmp.norm()
+        pol_theta = ti.asin(tmp[1] / pol_r)
+        sdf = pol_r-(3-3*ti.sin(pol_theta)+(ti.sin(pol_theta)*ti.sqrt(ti.abs(ti.cos(pol_theta)))/(ti.sin(pol_theta)+1.6)))
+        if sdf < 0:
+            z_axis = sz * ti.exp(-(ti.pow(I[0]/sz,2)/0.25+ti.pow(I[1]/sz,2)/0.25)/2) / (0.5 * ti.math.pi)
+            for i in range(-z_axis, z_axis):
+                scene.set_voxel(pos + ivec3(I, i), 2, color)
+
 
 @ti.func
 def create_butterfly(pos, sz, axis, angle, color):
@@ -35,11 +44,6 @@ def create_butterfly(pos, sz, axis, angle, color):
 def create_moutain(pos, sz, height):
     pass
 
-@ti.func
-def create_heart(pos, sz, rotate, color):
-    pass
-
-
 @ti.kernel
 def initialize_voxels():
     # Your code here! :-)
@@ -51,7 +55,7 @@ def initialize_voxels():
     create_butterfly(vec3(10,0,0), 15, vec3(-1, 1, 1), radians(30), vec3(0.1, 0.3, 0.8))
     create_butterfly(vec3(-20, 0, 0), 10, vec3(1, 1, 1), radians(-30), vec3(0.8, 0.2, 0.1))
     if night_mode:
-        create_moon(ivec3(40, 40, -40), 10, vec3(1.0, 1.0, 0.1))
+        create_heart(ivec3(40, 40, -40), 15, vec3(0.8, 0.1, 0.2)/exposure)
 
 initialize_voxels()
 
